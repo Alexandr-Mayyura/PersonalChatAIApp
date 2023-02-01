3b0198b6e371c64dc5f7ba6cee3976c3c25f0043
Alexandr-Mayyura <sonickill@yandex.ru>
2023-02-01 16:26:09 +0300

    Initial Commit

---
+++ PersonalChatAIApp/PersonalChatAIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalChatAIAppApp.swift
+//  PersonalChatAIApp
+//
+//  Created by Aleksandr Mayyura on 01.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct PersonalChatAIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
