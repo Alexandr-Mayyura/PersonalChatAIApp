1ff529ee1d48cb51c4944aed6e9ca5dc5e9a8c2b
Alexandr-Mayyura <sonickill@yandex.ru>
2023-02-01 20:04:16 +0300

    first commit

--- PersonalChatAIApp/ContentView.swift
+++ PersonalChatAIApp/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var viewModel = PersonalChatAIViewModel()
+    @State var text = "Привет"
+    @State var model = [String]()
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ScrollView {
+                VStack(alignment: .leading) {
+                    ForEach(model, id: \.self) { string in
+                        
+                        Text(string)
+                        
+                    }
+                }
+            
+            }
+            Spacer()
+            
+            HStack {
+                TextField("Type here...", text: $text)
+                Button("Send") {
+                    send()
+                }
+            }
+        }
+        
+        
+        .onAppear{
+            viewModel.setup()
         }
         .padding()
+    }
+    
+    func send() {
+        guard !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        model.append("Me:\n\(text)\n")
+        viewModel.send(text: text) { response in
+            DispatchQueue.main.async {
+                self.model.append("ChatGPT:\n" + response + "\n")
+                self.text = ""
+                print(response)
+            }
+        }
     }
 }
 
---
+++ PersonalChatAIApp/PersonalChatAIViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  PersonalChatAIViewModel.swift
+//  PersonalChatAIApp
+//
+//  Created by Aleksandr Mayyura on 01.02.2023.
+//
+import OpenAISwift
+import SwiftUI
+
+final class PersonalChatAIViewModel: ObservableObject {
+    init() {}
+    
+    private var client: OpenAISwift?
+    
+    func setup() {
+        client = OpenAISwift(authToken: Token.myToken.rawValue)
+        
+    }
+    
+    func send(text:String, completion: @escaping (String) -> Void) {
+        client?.sendCompletion(with: text, maxTokens: 1000) { result in
+            switch result {
+            case .success(let model):
+                let output = model.choices.first?.text ?? ""
+                completion(output)
+            case .failure(let error):
+                print(error.localizedDescription)
+                break
+            }
+        }
+    }
+}
