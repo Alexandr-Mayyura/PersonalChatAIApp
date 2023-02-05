fb32dc153dcf3f28c1ffd19aa8f37e24b62d017b
Alexandr-Mayyura <sonickill@yandex.ru>
2023-02-05 13:55:56 +0300

    add Token file

---
+++ PersonalChatAIApp/ConfigSecret.swift
@@ -0,0 +1,10 @@
+//
+//  ConfigSecret.swift
+//  PersonalChatAIApp
+//
+//  Created by Aleksandr Mayyura on 05.02.2023.
+//
+
+enum Token: String {
+    case myToken = "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"
+}
